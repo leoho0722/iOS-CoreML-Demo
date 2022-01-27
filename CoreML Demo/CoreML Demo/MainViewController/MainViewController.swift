260b168bc8e7b36565e8bee8c3530277e9b0a266
leoho0722 <leo160918@gmail.com>
2022-01-27 21:56:59 +0800

    0127 Commit

---
+++ CoreML Demo/CoreML Demo/MainViewController/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  CoreML Demo
+//
+//  Created by Leo Ho on 2022/1/26.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(12345)
+    }
+
+}
